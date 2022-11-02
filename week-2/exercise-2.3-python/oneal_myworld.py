d6902fcb98ace0fc857928fbab91622c3c2577a3
Chad ONeal <chadoneal3@gmail.com>
2022-11-02 12:58:54 -0400

    week-3

---
+++ week-2/exercise-2.3-python/oneal_myworld.py
@@ -0,0 +1,9 @@
+"""
+    Title: Exercise 2.3 myworld.py
+    Author: Chad ONeal
+    Date: 10/27/22
+    Description: Exercise 2.3
+"""
+
+myName = "Chad ONeal's"
+print("You are now in " + myName + "'s world!")
