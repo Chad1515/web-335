0379fe0688bbfa8100d9d2e32942c9dcf9ad76cd
Chad ONeal <cdoneal@my365.bellevue.edu>
2022-11-15 15:33:50 -0500

    week_2

---
+++ week_2/exercise_2.3_python/oneal_myworld.py
@@ -0,0 +1,20 @@
+<<<<<<< HEAD
+"""
+    Title: Exercise 2.3 myworld.py
+    Author: Chad ONeal
+    Date: 10/27/22
+    Description: Exercise 2.3
+"""
+
+myName = "Chad ONeal's"
+=======
+"""
+    Title: Exercise 2.3 myworld.py
+    Author: Chad ONeal
+    Date: 10/27/22
+    Description: Exercise 2.3
+"""
+
+myName = "Chad ONeal's"
+>>>>>>> 7f4f61d7c33ae3846a6784bc435dcbbae2a07045
+print("You are now in " + myName + "'s world!")
