3c70eccd04acf6df469f67f79b28051bdc7120c7
Chad ONeal <cdoneal@my365.bellevue.edu>
2022-11-21 19:06:34 -0500

    week_5

--- week_5/exercise_5.3_python_conditionals/oneal_hobbies.py
+++ week_5/exercise_5.3_python_conditionals/oneal_hobbies.py
@@ -4,18 +4,22 @@ Author: Chad ONeal
 Date: 11/15/2022
 Description: Python Conditionals/Lists/Loops assignment for WEB 335
 """
-
+# list of hobbies
 hobbies = ["hunting", "fishing", "camping", "hiking", "biking"]
 
+# print each hobby to console
 for hobby in hobbies:
     print(hobby)
 
-days = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"]
+# list of days of the week 
+days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 
+# print message for the corresponding day of the week
 for day in days:
-    if day == "sunday" or day == "saturday":
-        print("Today you are off. It is: " + day)
+    if day == "Sunday" or day == "Saturday":
+        print("Today you are off.  Go enjoy your hobbies! It is: " + day)
     else:
-        print("Today is a work day. It is: " + day)
+        print("Today is a work day. Better work Hard! It is: " + day)
 
+# enjoy message
 input("Enjoy!")
